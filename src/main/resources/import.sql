fd37d94f23bf07570d60267161f207f54fe1b1b4
GadelhaTI <gadelha.ti@gmail.com>
2023-01-28 21:41:46 -0300

    create schema

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
+-- CREATING SCHEMA
+CREATE SCHEMA IF NOT EXISTS nutrition;
+
 -- POPULING nutrition.food
 INSERT INTO nutrition.food (id, name) VALUES ('ba65d957-d0ab-426b-b23e-4b28e195c323', 'cereja') ON CONFLICT DO NOTHING;
 
