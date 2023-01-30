e78e4dc358f8c203d531bbc98611d96529557642
Gadelha TI <gadelha.ti@gmail.com>
2023-01-30 12:54:28 -0300

    change register add with a UserEntity

--- src/main/java/br/eti/gadelha/nutrition/controller/ControllerAuth.java
+++ src/main/java/br/eti/gadelha/nutrition/controller/ControllerAuth.java
@@ -1,11 +1,13 @@
 package br.eti.gadelha.nutrition.controller;
 
+import br.eti.gadelha.nutrition.persistence.payload.request.DTORequestUserEntity;
 import br.eti.gadelha.nutrition.persistence.payload.response.DTOResponseAuth;
 import br.eti.gadelha.nutrition.persistence.payload.request.DTORequestAuth;
 import br.eti.gadelha.nutrition.persistence.payload.response.DTOResponseUserEntity;
 import br.eti.gadelha.nutrition.service.ServiceAuth;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -22,7 +24,7 @@ public class ControllerAuth {
         return ResponseEntity.accepted().body(serviceAuth.login(value));
     }
     @PostMapping("/register")
-    public ResponseEntity<DTOResponseUserEntity> register(@RequestBody @Valid DTORequestAuth value){
+    public ResponseEntity<DTOResponseUserEntity> register(@RequestBody @Valid DTORequestUserEntity value){
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/auth/register").toUriString());
         return ResponseEntity.created(uri).body(serviceAuth.register(value));
     }
