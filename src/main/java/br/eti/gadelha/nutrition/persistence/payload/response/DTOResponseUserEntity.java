1502c9d71079aa2801461404c104efaaf972fdb9
Gadelha TI <gadelha.ti@gmail.com>
2023-02-12 17:29:32 -0300

    remove password on response

--- src/main/java/br/eti/gadelha/nutrition/persistence/payload/response/DTOResponseUserEntity.java
+++ src/main/java/br/eti/gadelha/nutrition/persistence/payload/response/DTOResponseUserEntity.java
@@ -4,9 +4,6 @@ import br.eti.gadelha.nutrition.persistence.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import java.util.Collection;
 import java.util.UUID;
 
